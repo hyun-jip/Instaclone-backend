1f33b9c097a2da14258b4d97dfd127e3aff4e849
hyun-jip <frank910@naver.com>
2021-06-29 23:46:25 +0900

    21-06-29

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
+import { graphqlUploadExpress } from "graphql-upload";
 
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
+  uploads: false,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
@@ -18,6 +20,7 @@ const apollo = new ApolloServer({
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(graphqlUploadExpress());
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
